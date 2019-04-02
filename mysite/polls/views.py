2006f1f687559eaf28845cd0f91b346c9b576634
Arun Kumar Pyasi <expertaruncorp@gmail.com>
2019-04-02 16:11:07 +0545

    django model created with template and static applied

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
 # Create your views here.
 
 def home(requests):
-    body = "<html><body><h1>Hello this is poll</h1></body></html>"
-    return HttpResponse(body)
+    
+    return render(requests, 'polls/index.html')
