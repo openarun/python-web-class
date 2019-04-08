6b3af2221e60051cdcb8a26c39c58907127fa160
Arun Kumar Pyasi <expertaruncorp@gmail.com>
2019-04-08 21:14:53 +0545

    polls show questions

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('<int:question_id>/', views.detail),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 # Create your views here.
+from .models import Question,Choice
 
 def home(requests):
-    
-    return render(requests, 'polls/index.html')
+    all_questions = Question.objects.all()
+    data = {
+        'questions_list' : all_questions
+    }
+    return render(requests, 'polls/index.html', context=data)
+
+def detail(requests,question_id):
+
+    question = Question.objects.get(pk=question_id)
+    data = {
+        'question': question
+    }
+    return render(requests, 'polls/detail.html', context=data)
